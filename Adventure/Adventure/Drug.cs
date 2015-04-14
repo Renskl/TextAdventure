94816f2a33aea6e46692754fb24a78193e28375e
Rens <renskloosterhuis93@gmail.com>
2015-04-14 19:48:16 +0200

    spare some change

---
+++ Adventure/Adventure/Drug.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Adventure
+{
+	public enum DrugsEffects 
+	{
+		HIGH,
+		BAD,
+		PSYCH,
+		STONED,
+		CONNECTED
+
+	}
+
+	public abstract class Drug
+	{
+		protected string name;
+		protected double value;
+
+		public Drug (string name, double value)
+		{
+			this.name = name;
+			this.value = value;
+		}
+		public string Name {
+			
+			get;
+			set;
+		}
+		public double Value {
+			get;
+			set;
+		}
+	}
+}
+
