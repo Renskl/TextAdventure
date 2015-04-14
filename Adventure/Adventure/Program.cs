ca2360a19564896f3d59cc0185fb5713406c0eaf
Matthias Calis <matthiascalis@gmail.com>
2015-04-14 18:30:47 +0200

    Reset the project

--- Adventure/Adventure/Program.cs
+++ Adventure/Adventure/Program.cs
@@ -3,63 +3,14 @@ using System.Collections.Generic;
 
 namespace Adventure
 {
-	class Program
-	{
+    class Program
+    {
         public static bool isRunning = true;
-        public static EKeys command;
-        public static List<IUpdate> updateables = new List<IUpdate>();
 
-		public static Menu mainMenu;
 
-        public static void Main (string[] args)
-		{
-			mainMenu = new Menu ();
-            while (isRunning)
-            {
-                Update(Input());
-                Render();
-            }
-		}
-
-        private static EKeys Input()
-        {
-            ConsoleKeyInfo cki = Console.ReadKey();
-            command = EKeys.NONE;
-            if (cki.Key == ConsoleKey.UpArrow)
-            {
-                command = EKeys.UP;
-            }
-            if (cki.Key == ConsoleKey.DownArrow)
-            {
-                command = EKeys.DOWN;
-            }
-            if (cki.Key == ConsoleKey.Enter)
-            {
-                command = EKeys.CONFIRM;
-            }
-            if (cki.Key == ConsoleKey.Escape)
-            {
-                command = EKeys.CANCEL;
-            }
-            return command;
-        }
-
-        private static void Update(EKeys Command)
-        {
-            foreach (IUpdate u in updateables)
-            {
-                u.Update(Command);
-            }
-        }
-
-        private static void Render()
+        public static void Main(string[] args)
         {
 
         }
-
-        public static void Register(IUpdate updateable)
-        {
-            updateables.Add(updateable);
-        }
-	}
+    }
 }
