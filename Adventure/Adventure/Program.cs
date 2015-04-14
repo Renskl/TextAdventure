9d56744293ad79f4e2becc5dd95e8a1d50809f8c
Matthias Calis <matthiascalis@gmail.com>
2015-04-14 19:17:38 +0200

    dignetjes

--- Adventure/Adventure/Program.cs
+++ Adventure/Adventure/Program.cs
@@ -15,54 +15,32 @@ namespace Adventure
 
         public static void Main(string[] args)
         {
-            using (var game = new GameWindow())
+            var game = new GameWindow();
+            GL.Viewport(0, 0, game.Width, game.Height);
+            game.RenderFrame += (sender, e) =>
             {
-                game.Load += (sender, e) =>
-                {
-                    // setup settings, load textures, sounds
-                    game.VSync = VSyncMode.On;
-                };
+                // render graphics
+                GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
 
-                game.Resize += (sender, e) =>
-                {
-                    GL.Viewport(0, 0, game.Width, game.Height);
-                };
+                GL.MatrixMode(MatrixMode.Projection);
+                GL.LoadIdentity();
+                GL.Ortho(-1.0, 1.0, -1.0, 1.0, 0.0, 4.0);
 
-                game.UpdateFrame += (sender, e) =>
-                {
-                    // add game logic, input handling
-                    if (game.Keyboard[Key.Escape])
-                    {
-                        game.Exit();
-                    }
-                };
+                GL.Begin(PrimitiveType.Triangles);
 
-                game.RenderFrame += (sender, e) =>
-                {
-                    // render graphics
-                    GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+                GL.Color3(Color.MidnightBlue);
+                GL.Vertex2(-1.0f, 1.0f);
+                GL.Color3(Color.SpringGreen);
+                GL.Vertex2(0.0f, -1.0f);
+                GL.Color3(Color.Ivory);
+                GL.Vertex2(1.0f, 1.0f);
 
-                    GL.MatrixMode(MatrixMode.Projection);
-                    GL.LoadIdentity();
-                    GL.Ortho(-1.0, 1.0, -1.0, 1.0, 0.0, 4.0);
+                GL.End();
 
-                    GL.Begin(PrimitiveType.Triangles);
-
-                    GL.Color3(Color.MidnightBlue);
-                    GL.Vertex2(-1.0f, 1.0f);
-                    GL.Color3(Color.SpringGreen);
-                    GL.Vertex2(0.0f, -1.0f);
-                    GL.Color3(Color.Ivory);
-                    GL.Vertex2(1.0f, 1.0f);
-
-                    GL.End();
-
-                    game.SwapBuffers();
-                };
-
-                // Run the game at 60 updates per second
-                game.Run(60.0);
-            }
+                game.SwapBuffers();
+            };
+            // Run the game at 60 updates per second
+            game.Run(60.0);
         }
     }
 }
